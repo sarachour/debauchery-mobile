03b630f51d686a24a1f24997851186adcb022e3e
sachour <sachour@mit.edu>
2014-07-31 13:07:15 -0400

    modified main activity to support drawing

--- src/com/debauchery/MainActivity.java
+++ src/com/debauchery/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends ActionBarActivity {
 				startActivity(i);
 			}
 		});
-		draw.setOnClickListener(new OnClickListener(){
+		describe.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
