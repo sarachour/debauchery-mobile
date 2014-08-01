e05aa37eb7c7feed448c96952e857b76b95d5104
sachour <sachour@mit.edu>
2014-08-01 11:33:20 -0400

    added sp,e styling

--- gen/com/debauchery/R.java
+++ gen/com/debauchery/R.java
@@ -1078,7 +1078,9 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int card_shape=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
+        public static final int inset_shape=0x7f020059;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1092,16 +1094,16 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050050;
+        public static final int action_settings=0x7f050051;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f050045;
-        public static final int cv_canv=0x7f05004e;
-        public static final int cv_done=0x7f05004f;
-        public static final int cv_prompt=0x7f05004d;
+        public static final int cv_canv=0x7f05004f;
+        public static final int cv_done=0x7f050050;
+        public static final int cv_prompt=0x7f05004e;
         public static final int default_activity_button=0x7f050028;
         public static final int describe_view=0x7f05003c;
         public static final int dialog=0x7f05000e;
@@ -1136,10 +1138,11 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
-        public static final int rv_display=0x7f05004a;
-        public static final int rv_display_layout=0x7f05004b;
-        public static final int rv_done=0x7f05004c;
-        public static final int rv_label_1=0x7f050049;
+        public static final int review_view=0x7f050049;
+        public static final int rv_display=0x7f05004b;
+        public static final int rv_display_layout=0x7f05004c;
+        public static final int rv_done=0x7f05004d;
+        public static final int rv_label_1=0x7f05004a;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1301,6 +1304,7 @@ containing a value of this type.
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f0b008c;
+        public static final int CardStyle=0x7f0b008e;
         public static final int InsetStyle=0x7f0b008d;
         /**  Mimic text appearance in select_dialog_item.xml 
          */
