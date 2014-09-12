02325a15aea322d0bb4b05ce9bccf8d0f49471f5
sachour <sachour@mit.edu>
2014-09-12 12:01:39 -0400

    added thing

--- src/com/debauchery/DrawActivity.java
+++ src/com/debauchery/DrawActivity.java
@@ -24,6 +24,9 @@ import android.os.Parcel;
 import android.support.v7.app.ActionBarActivity;
 import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -40,6 +43,43 @@ import android.widget.ViewSwitcher;
 public class DrawActivity extends ActionBarActivity {
 	PersistantStateDatabase db;
 	SketchPad sketchpad;
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+		Intent i;
+	    switch (item.getItemId()) {
+	        case R.id.action_next:
+	        	if(sketchpad.getData().size() == 0){
+					new AlertDialog.Builder(this)
+				    .setTitle("No Drawing Found")
+				    .setMessage("Please draw something.")
+				    .setIcon(android.R.drawable.ic_dialog_alert)
+				     .show();
+					return true;
+				}
+	        	saveSketchToDatabase();
+				i = new Intent(getApplicationContext(), ShowActivity.class);
+				db.init(db.getTurn()+1, Globals.SHOW_PHASE);
+				startActivity(i);
+				
+	            return true;
+	        case R.id.action_prev:
+	        	if(db.getTurn() > 0){
+	        		saveSketchToDatabase();
+					i = new Intent(getApplicationContext(), PromptActivity.class);
+					db.init(db.getTurn(), Globals.PROMPT_PHASE);
+					startActivity(i);
+	        	}
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.action, menu);
+	    return  super.onCreateOptionsMenu(menu);
+	}
 	
 	private void loadSketchFromDatabase(){
 		sketchpad.loadData(db.getSketch());
@@ -68,32 +108,11 @@ public class DrawActivity extends ActionBarActivity {
 		db = new PersistantStateDatabase(this);
 		db.init(db.getTurn(), Globals.DRAW_PHASE);
 		
-		final Button done = (Button) findViewById(R.id.sa_sketch_done);
+		
 		sketchpad =  (SketchPad) findViewById(R.id.fd_sketchpad);
 		
 		loadSketchFromDatabase();
 		
-		done.setOnClickListener(new OnClickListener(){
-
-			@Override
-			public void onClick(View v) {
-				
-				if(sketchpad.getData().size() == 0){
-					new AlertDialog.Builder(v.getContext())
-				    .setTitle("No Drawing Found")
-				    .setMessage("Please draw something.")
-				    .setIcon(android.R.drawable.ic_dialog_alert)
-				     .show();
-					return;
-				}
-				saveSketchToDatabase();
-				Intent i = new Intent(getApplicationContext(), ShowActivity.class);
-				db.init(db.getTurn()+1, Globals.SHOW_PHASE);
-				startActivity(i);
-				
-			}
-			
-		});
 		
 	}
 }
